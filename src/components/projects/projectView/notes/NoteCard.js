42318165fefa3b54891610f04fb78ba9beb27d8c
brandonjon26 <brandonjon26@gmail.com>
2021-04-29 15:10:05 -0500

    note cards are set up for when i display the list

---
+++ src/components/projects/projectView/notes/NoteCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import "/Note.css";
+import { Link } from "react-router-dom";
+
+export const NoteCard = ({ note, handleDeleteNote }) => {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <div className="notes">
+                    <div className="note">
+                        <p>Note: {note.text}</p>
+                        <button type="button" onClick={() => handleDeleteNote(note.id)}>Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

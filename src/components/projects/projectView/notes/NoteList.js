4c88eb19373b6d852408222400585f9fa7f7f1a0
brandonjon26 <brandonjon26@gmail.com>
2021-04-29 15:44:42 -0500

    note cards now display on the list

---
+++ src/components/projects/projectView/notes/NoteList.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { NoteCard } from "./NoteCard";
+import { getAllNotesByProject, deleteNote } from "../../../../modules/NoteManager";
+import { useHistory } from "react-router";
+
+export const NoteList = ({ projectId }) => {
+    
+    const [notes, setNotes] = useState([]);
+
+    const history = useHistory();
+
+    const getNotesByProjectId = (id) => {
+        return getAllNotesByProject(id)
+            .then(notesFromAPI => {
+                setNotes(notesFromAPI)
+            });
+    };
+
+    const handleDeleteNote = id => {
+        deleteNote(id)
+            .then(() => getAllNotesByProject(projectId).then(setNotes));
+    };
+
+    useEffect(() => {
+        getNotesByProjectId(projectId);
+    }, [projectId]);
+
+    return (
+        <>
+            <div id="notes" className="container-cards">
+                <section className="section-content">
+                    <button type="button"
+                        className="btn"
+                        onClick={() => { history.push("/notes/create") }}>
+                        Add A Note
+                    </button>
+                    {notes.map(note => <NoteCard 
+                        key={note.id}
+                        note={note}
+                        handleDeleteNote={handleDeleteNote}
+                        />)}
+                </section>
+            </div>
+        </>
+    )
+}
---
+++ src/modules/NoteManager.js
@@ -0,0 +1,17 @@
+const remoteURL = "http://localhost:8088"
+
+export const getAllNotesByProject = (projectId) => {
+    return fetch(`${remoteURL}/notes?projectId=${projectId}`)
+        .then(result => result.json())
+}
+
+export const getNoteById = (id) => {
+    return fetch(`${remoteURL}/notes/${id}`)
+        .then(res => res.json())
+}
+
+export const deleteNote = (id) => {
+    return fetch(`${remoteURL}/notes/${id}`, {
+        method: "DELETE"
+    }).then(result => result.json())
+}
